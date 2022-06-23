05caf9635aef4d75a7894a919b1c1330c0033379
Syed Hassam Kazmi <59208947+Hassamkazmi@users.noreply.github.com>
2022-06-23 12:27:38 +0500

    Styling popup

--- components/Layout/components/Popup/Popup.js
+++ components/Layout/components/Popup/Popup.js
@@ -5,7 +5,7 @@ import crossIcon from '../../../../public/assets/images/crossIcon.svg'
 
 export default function Popup({content, handleClose}){
     return(
-        <div className="fixed w-1/2 bg-white h-[200px] border-8 inset-0 m-auto z-10  rounded-lg" >
+        <div className="fixed w-1/2 bg-white h-[200px] border-8 inset-0 m-auto z-10 rounded-lg" >
             <div className="absolute right-[10px] top-[10px]">
                 <button
                     onClick={handleClose}
