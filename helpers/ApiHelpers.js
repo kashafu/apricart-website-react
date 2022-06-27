c8209fd139ed9a756cd8b03b198e5ae4d350b0a7
MujtabaAhmed2000 <m.mujtabaahmed2000@gmail.com>
2022-06-27 14:30:55 +0500

    product detail page made

--- helpers/ApiHelpers.js
+++ helpers/ApiHelpers.js
@@ -1,4 +1,5 @@
 import Cookies from 'universal-cookie';
+let isNode = require('detect-node')
 
 const cookies = new Cookies();
 
@@ -6,13 +7,15 @@ export const getGeneralApiParams = () => {
     let token = cookies.get('cookies-token')
     // city cookies is being set in TopBar.js
     let city = cookies.get('cities')
-    let latitude = 0 
+    let latitude = 0
     let longitude = 0
     let userId
-    let headers = {}
+    let headers = {
+        "Content-Type": "application/json",
+    }
 
     // if user is logged in
-    if(token){
+    if (token) {
         userId = cookies.get('cookies-userId')
         // if user has a selected address, use that addresses's latitude longitude
         // TODO
@@ -23,19 +26,21 @@ export const getGeneralApiParams = () => {
         }
     }
     // if its a guest
-    else{
+    else {
         userId = cookies.get('guestUserId')
         // if location enabled, use browser latitude and longitude, if not enabled, send 0 by default
-        navigator.geolocation.getCurrentPosition((position)=> {
-            latitude= position.coords.latitude
-            longitude= position.coords.longitude
-        })
+        if (!isNode) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                latitude = position.coords.latitude
+                longitude = position.coords.longitude
+            })
+        }
         headers = {
             "Content-Type": "application/json",
         }
     }
 
-    return({
+    return ({
         'city': city,
         'latitude': latitude,
         'longitude': longitude,
--- pages/details/[id].js
+++ pages/details/[id].js
@@ -1,196 +1,235 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Catagory1 from "../../components/Layout/components/Categories/Categories";
+import Categories from "../../components/Layout/components/Categories/Categories";
 import RelatedProduct from "../../components/Layout/components/RelatedProduct/RelatedProduct";
-let base_url_api = "https://staging.apricart.pk/v1";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/cart.slice";
 import Cookies from 'universal-cookie';
+import { base_url_api } from '../../information.json'
+import { getGeneralApiParams } from '../../helpers/ApiHelpers'
+import axios from "axios";
+
+export default function Post({ product }) {
+	let [num, setNum] = useState(1);
+	const dispatch = useDispatch();
+	const cookies = new Cookies();
+	var token = cookies.get("cookies-token");
+
+	const [categories, setCategories] = useState(null)
+
+	useEffect(()=>{
+		getCategoriesApi()
+	}, [])
+
+	const getCategoriesApi = async () => {
+		let {city, headers} = getGeneralApiParams()
+		let url = base_url_api + '/catalog/categories?level=all&client_type=apricart&city=' + city
+		
+		try {
+			let response = await axios.get(url, 
+				{
+					headers: headers
+				}
+			)
+			setCategories(response.data.data)
+		} catch (error) {
+			console.log(error)
+		}
+
+	}
+
+	const router = useRouter();
+	if (router.isFallback) {
+		return (
+			<div>
+				<h2>Loading Page Data...</h2>
+			</div>
+		);
+	}
 
 
-export default function Post({ postData }) {
-  let [num, setNum] = useState(1);
-  const dispatch = useDispatch();
-  const cookies = new Cookies();
-  var token = cookies.get("cookies-token");
 
+	let incNum = () => {
+		if (num < 5) {
+			setNum(Number(num) + 1);
+		}
+	};
+	let decNum = () => {
+		if (num > 0) {
+			setNum(num - 1);
+		}
+	};
+	const Cartapi = (list) => {
+		const data =
+		{
+			cart:
+				[
+					{
+						sku: list.sku,
+						qty: "1"
+					}
+				]
+		}
+		console.log(data.cart)
 
-  const router = useRouter();
-  if (router.isFallback) {
-    return (
-      <div>
-        <h2>Loading Page Data...</h2>
-      </div>
-    );
-  }
+		if (token) {
+			console.log("AVG ")
+			console.log(list.sku);
+			const response = axios.post('https://staging.apricart.pk/v1/order/cart/save?city=karachi&lang=en', data, {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: "Bearer " + cookies.get("cookies-token"),
+				},
+			}
 
-  
-  let incNum = () => {
-    if (num < 5) {
-      setNum(Number(num) + 1);
-    }
-  };
-  let decNum = () => {
-    if (num > 0) {
-      setNum(num - 1);
-    }
-  };
-  const Cartapi =(list)=>{
-    const data =
-    {cart : 
-     [
-         {
-          sku: list.sku,
-          qty: "1"
-         }
-     ]}
-     console.log(data.cart)
-    
-    if (token){
-      console.log("AVG ")
-      console.log(list.sku);
-      const response =  axios.post( 'https://staging.apricart.pk/v1/order/cart/save?city=karachi&lang=en' ,data,{
-        headers:{
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + cookies.get("cookies-token"),
-        },
-      } 
-      
-      );
-    }
+			);
+		}
 
-  }
+	}
 
-  return (
-    <div>
-      <section className="popular_sec">
-        <div className="container-fluid">
-          <div className="row">
-          <div className="col-12 col-sm-2  col-md-2  col-lg-3  col-xl-2  col-xxl-2">
-              <Catagory1 />
-            </div>
-            <div className="col-12 col-sm-12  col-md-10  col-lg-9  col-xl-10  col-xxl-10 parot">
-              {postData.map((catagory) => {
-                const { productImageUrl, categoryleafName, minQty, maxQty } =
-                  catagory;
-                return (
-                  <>
-                   
-                    <section className="ContentSec">
-                      <div className="container-fluid">
-                        <div className="prothreeHead">
-                          <ol className="breadcrumb">
-                            <li>
-                              {" "}
-                              <a passHref="">Home</a>{" "}
-                            </li>
-                            <li>
-                              <a passHref="">{categoryleafName}</a>
-                            </li>
-                            <li>
-                              <a passHref="">{catagory.title}</a>
-                            </li>
-                          </ol>
-                        </div>
-                      </div>
-                    </section>
-                    <section className="productdet_sec">
-                      <div className="container">
-                        <div className="row">
-                          <div className="col-12 col-sm-4  col-md-4  col-lg-4  col-xl-4  col-xxl-4">
-                            <div className="proDimg">
-                              <img
-                                src={productImageUrl}
-                                classNameName="img-fluid"
-                                alt=""
-                              />
-                            </div>
-                          </div>
-                          <div className="col-12 col-sm-6  col-md-6  col-lg-6  col-xl-6  col-xxl-6">
-                            <div className="productD_head">
-                              <h3>{catagory.title}</h3>
-                              <hr />
-                              <h4>Rs. {catagory.currentPrice}</h4>
-                              <hr />
-                              <div className="productD_para">
-                                <p>{catagory.description}</p>
-                                <span>{catagory.sku}</span>
-                              </div>
-                              <ul className="detail-pro">
-                                <li>
-                                  <div className="prodetails_size">
-                                    <select name="product" id="productd">
-                                      <option value="1">1 KG</option>
-                                      <option value="2">2 KG</option>
-                                      <option value="3">3 KG</option>
-                                    </select>
-                                  </div>
-                                </li>
-                                <li>
-                                  {catagory.inStock == true ? (
-                                    <div
-                                    className="pro_btn2"
-                                    onClick={() =>{
-                                      Cartapi(catagory);
-                                      dispatch(addToCart(catagory))}
-                                    }
-                                  >
-                                    <a
-                                      href="#"
-                                      className="btn btn-primary chane chane1"
-                                    >
-                                      Add to Cart
-                                    </a>
-                                  </div>
-                                  ) : (
-                                    <>
-                                      <div className="pro_btn2">
-                                        <a
-                                          passHref="#"
-                                          className="btn btn chane chane1 secondary"
-                                          disabled
-                                        >
-                                          Out of stock
-                                        </a>
-                                      </div>
-                                    </>
-                                  )}
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </section>
-                  </>
-                );
-              })}
-              <RelatedProduct />
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+	return (
+		<div>
+			{product.data.length > 0 ?
+				(
+					<div>
+						<section className="popular_sec">
+							<div className="container-fluid">
+								<div className="row">
+									<div className="col-12 col-sm-2  col-md-2  col-lg-3  col-xl-2  col-xxl-2">
+										{categories && (
+											<div>
+												<Categories
+													categories={categories}
+												/>
+											</div>
+										)}
+									</div>
+									<div className="col-12 col-sm-12  col-md-10  col-lg-9  col-xl-10  col-xxl-10 parot">
+										<>
+											<section className="ContentSec">
+												<div className="container-fluid">
+													<div className="prothreeHead">
+														<ol className="breadcrumb">
+															<li>
+																{" "}
+																<a passHref="">Home</a>{" "}
+															</li>
+															<li>
+																<a passHref="">{product.data[0].categoryleafName}</a>
+															</li>
+															<li>
+																<a passHref="">{product.data[0].title}</a>
+															</li>
+														</ol>
+													</div>
+												</div>
+											</section>
+											<section className="productdet_sec">
+												<div className="container">
+													<div className="row">
+														<div className="col-12 col-sm-4  col-md-4  col-lg-4  col-xl-4  col-xxl-4">
+															<div className="proDimg">
+																<img
+																	src={product.data[0].productImageUrl}
+																	classNameName="img-fluid"
+																	alt=""
+																/>
+															</div>
+														</div>
+														<div className="col-12 col-sm-6  col-md-6  col-lg-6  col-xl-6  col-xxl-6">
+															<div className="productD_head">
+																<h3>{product.data[0].title}</h3>
+																<hr />
+																<h4>Rs. {product.data[0].currentPrice}</h4>
+																<hr />
+																<div className="productD_para">
+																	<p>{product.data[0].description}</p>
+																	<span>{product.data[0].sku}</span>
+																</div>
+																<ul className="detail-pro">
+																	<li>
+																		{product.data[0].inStock == true ? (
+																			<div
+																				className="pro_btn2"
+																				onClick={() => {
+																					Cartapi(catagory);
+																					dispatch(addToCart(catagory))
+																				}
+																				}
+																			>
+																				<a
+																					href="#"
+																					className="btn btn-primary chane chane1"
+																				>
+																					Add to Cart
+																				</a>
+																			</div>
+																		) : (
+																			<>
+																				<div className="pro_btn2">
+																					<a
+																						passHref="#"
+																						className="btn btn chane chane1 secondary"
+																						disabled
+																					>
+																						Out of stock
+																					</a>
+																				</div>
+																			</>
+																		)}
+																	</li>
+																</ul>
+															</div>
+														</div>
+													</div>
+												</div>
+											</section>
+										</>
+										<RelatedProduct />
+									</div>
+								</div>
+							</div>
+						</section>
+					</div>
+				)
+				:
+				(
+					<div>
+						<p>
+							Item does not exist
+						</p>
+					</div>
+				)
+			}
+		</div>
+	);
 }
 
 export async function getStaticPaths() {
-  const paths = ["/details/[id]", "/details/[slug]"];
-  return { paths, fallback: true };
+	const paths = ["/details/[id]", "/details/[slug]"];
+	return { paths, fallback: true };
 }
 
 export async function getStaticProps({ query, params }) {
-  const { id } = query || params;
+	const { id } = query || params;
+	let { headers } = getGeneralApiParams()
+	let city = 'karachi'
+	let url = base_url_api + '/catalog/products/detail?id=' + id + '&city=' + city + '&lang=en&client_type=apricart'
+	let product = null
 
-  //const res = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
-  const res = await fetch(
-    "https://cbe.apricart.pk/v1/catalog/products/detail?id=" + id 
-  );
-  const alldata = await res.json();
-  const postData = alldata.data;
-  return {
-    props: {
-      postData,
-    },
-  };
+	try {
+		let response = await axios.get(url,
+			{
+				headers: headers
+			})
+		product = response.data;
+	} catch (error) {
+		console.log(error)
+	}
+
+	return {
+		props: {
+			product,
+		},
+	};
 }
