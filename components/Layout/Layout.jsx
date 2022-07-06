dd0e6e9d19ea57251ed147efa93e4178bd7fcd08
MujtabaAhmed2000 <m.mujtabaahmed2000@gmail.com>
2022-07-06 18:44:44 +0500

    search bar scrollable

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -13,7 +13,7 @@ export default function Layout(props) {
     };
     return (
         <AppContext.Provider value={{ appState, handleAppState }}>
-            <div className="flex flex-col min-h-screen min-w-screen">
+            <div className="flex flex-col min-h-screen w-screen">
                 <div>
                     <TopBar />
                     <Header />
--- components/Layout/components/SearchBar/SearchBar.js
+++ components/Layout/components/SearchBar/SearchBar.js
@@ -4,51 +4,51 @@ import { getGeneralApiParams } from "../../../../helpers/ApiHelpers"
 import { base_url_api } from '../../../../information.json'
 import SingleProduct from '../Products/SingleProduct'
 
-export default function SearchBar(){
+export default function SearchBar() {
     const [searchText, setSearchText] = useState('')
-	const [searchResults, setSearchResults] = useState([])
+    const [searchResults, setSearchResults] = useState([])
     const [showSearchResults, setShowSearchResults] = useState(false)
 
     const searchHandler = async (searchTerm) => {
-        if(searchTerm.length <= 3){
+        if (searchTerm.length <= 3) {
             setSearchResults([])
             setShowSearchResults(false)
             return
         }
-        let {city, userId, headers} = getGeneralApiParams()
+        let { city, userId, headers } = getGeneralApiParams()
         let url = base_url_api + '/catalog/products/search?page=1&size=20&term=' + searchTerm + '&category=&city=' + city + '&lang=en&userid=' + userId + '&client_type=apricart'
-		let searchResponse = await axios.get(
+        let searchResponse = await axios.get(
             url,
             {
                 'headers': headers
             }
-		)
+        )
         setShowSearchResults(true)
         setSearchResults(searchResponse.data.data)
-	}
+    }
 
-    return(
-        <div className="w-full">
+    return (
+        <div className="relative w-full">
             <input
+                className="p-2 w-full bg-slate-200 rounded-xl font-bold"
                 type={'search'}
-                className="w-full p-2 bg-slate-200 rounded-xl font-bold"
                 value={searchText}
-                onChange={(e)=>{
+                onChange={(e) => {
                     setSearchText(e.target.value)
                     searchHandler(e.target.value)
                 }}
                 placeholder='Search'
-                onBlur={()=>{
+                onBlur={() => {
                     setShowSearchResults(false)
                 }}
             />
             {showSearchResults && (
-                <div className="absolute bg-white w-full z-20 flex flex-col space-y-2 max-h-[300px] overflow-auto">
+                <div className="absolute z-20 w-full bg-white max-h-[350px] overflow-auto">
                     {searchResults.length > 0 ? (
                         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6'>
-                            {searchResults.map((product)=>{
-                                let {id} = product
-                                return(
+                            {searchResults.map((product) => {
+                                let { id } = product
+                                return (
                                     <div key={id}>
                                         <SingleProduct
                                             product={product}
@@ -57,9 +57,7 @@ export default function SearchBar(){
                                 )
                             })}
                         </div>
-                    )
-                    :
-                    (
+                    ) : (
                         <p>
                             Sorry, item does not exist
                         </p>
