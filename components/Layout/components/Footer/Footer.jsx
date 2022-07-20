47bced9a421970a90a6cc8438a442ad922c8fce4
Syed Hassam Kazmi <59208947+Hassamkazmi@users.noreply.github.com>
2022-07-20 16:22:41 +0500

    Update

--- components/Layout/components/Footer/Footer.jsx
+++ components/Layout/components/Footer/Footer.jsx
@@ -4,140 +4,75 @@ import Image from "next/image";
 
 export default function Layout() {
     return (
-        <section className="mainfooter">
-            <footer className="footer-distributed">
-                <div className="footer-left">
-                    <div className="contactlistinfo">
-                        <Image
-                            src="/assets/images/logo1.png"
-                            className="img-fluid"
-                            width={130}
-                            height={60}
-                            alt=""
-                        />
-                    </div>
+        <div className="bg-gray-200 inline-flex text-indigo-900 h-60 p-8 w-full">
+        <div className="w-4/12 text-indigo-900 justify-center items-center">           
+            <div className="inline-flex  h-1/3 my-1 justify-center items-center">
+             <i className="fa fa-map-marker"></i>
+             <p className="pl-2"><span className="font-bold">Address:</span><span>B-191, Block No. 5,</span> Gulshan-e-Iqbal, Karachi </p>
+            </div> <br />
+            <div className="inline-flex h-1/3 my-1 justify-center items-center">
+              <i className="fa fa-phone"></i>
+               <p className="pl-2"><span className="font-bold">Phone:</span><span className="highligh1">0304-1110195</span></p>
+          </div><br/>
+          <div className="inline-flex h-1/3 my-1 justify-center items-center">
+               <i className="fa fa-envelope"></i>
+               <p className="pl-2"><a href="mailto:support@apricart.pk"><span className="font-bold">Email:</span>support@apricart.pk</a></p><br />
+              </div>
+        </div>
+        
+           <div className="flex-row justify-center items-center w-4/12 my-2" >
 
-                    <p className="footer-links">
-                        <Link href={"/privacy-policy"} passHref>
-                            <a>Privacy Policy</a>
-                        </Link>
-                        <Link href={"/terms-of-use"} passHref>
-                            <a>Terms of Use</a>
-                        </Link>
-                        <Link href={"/faqs"} passHref>
-                            <a href="#">FAQs</a>
-                        </Link>
-                        <Link href={"/page-contact"} passHref>
-                            <a href="#">Contact Us</a>
-                        </Link>
-                    </p>
-                </div>
+           <a  className="justify-center items-center p-4" href="/privacy-policy">Privacy Policy</a><br/> 
+               <a  className="justify-center items-center p-4 " href="/terms-of-use">Terms of Use</a><br/>
+               <a  className="justify-center items-center p-4 " href="/faqs">FAQs</a><br/>
+               
+               <a  className="justify-center items-center p-4" href="/page-contact">Contact Us</a><br/>
+            
+           </div>
+           
+ <div className="w-4/12" > 
+           
+<div className="copytext">
+         <h3>Install Our App</h3>
+         <ul>
+            <li>
+               <a href="#"><img src="/assets/images/playstore-img.png"
+               // width={80} height={40} margin-right={50}
+               className="img-fluid"
+               alt=""
+               /></a>
+            </li>
+            <li>
+               <a href="#"><img src="/assets/images/appstore-img.png" 
+               // width={80} height={40}
+               className="img-fluid king"
+               alt=""
+               /></a>
+            </li>
+            
+         </ul>
+      </div>
+      <br/>
+      <p className="mb-2">Secured payment gateways</p>
+            <img src="/assets/images/payment-method.svg" 
+               // width={80} height={40}
+               className="mb-3"
+               alt=""
+               />
 
-                <div className="footer-center">
-                    <h3>Address</h3>
+      <div className="inline-flex">
 
-                    <div>
-                        <i className="fa fa-map-marker"></i>
-                        <p>
-                            <span>B-191, Block No. 5,</span> Gulshan-e-Iqbal,
-                            Karachi
-                        </p>
-                    </div>
+   <a  className="mx-2" href="https://www.facebook.com/apricartonlinegrocery/"><i className="fab fa-facebook"></i></a>
+   <a   className="mx-2" href="https://twitter.com/apricartpk?lang=en" ><i className="fab fa-twitter"></i></a>
+   <a  className="mx-2" href="https://pk.linkedin.com/company/apricartestores"><i className="fab fa-linkedin"></i></a>
+   <a  className="mx-2" href="#"><i className="fab fa-github"></i></a>
+   <a  className="mx-2" href="#"><i className="fab fa-instagram"></i></a>
+</div>
+{/* <!-- <p class="footer-company-name">Apricart E-Stores Pvt Ltd    <br>  All rights reserved</p> --> */}
+</div>
 
-                    <div>
-                        <i className="fa fa-phone"></i>
-                        <p>
-                            <span className="highligh1">0304-1110195</span>
-                        </p>
-                    </div>
 
-                    <div>
-                        <i className="fa fa-envelope"></i>
-                        <p>
-                            <a href="mailto:support@apricart.pk">
-                                support@apricart.pk
-                            </a>
-                        </p>
-                    </div>
-                </div>
-                <div className="footer-center">
-                    <h3>Click & Collect </h3>
 
-                    <div>
-                        <i className="fa fa-map-marker"></i>
-                        <p>
-                            <span>B-191, Block No. 5,</span> Gulshan-e-Iqbal,
-                            Karachi
-                        </p>
-                    </div>
-
-                    <div>
-                        <i className="fa fa-phone"></i>
-                        <p>
-                            <span className="highligh1">0304-1110195</span>
-                        </p>
-                    </div>
-
-                    <div>
-                        <i className="fa fa-envelope"></i>
-                        <p>
-                            <a href="mailto:support@apricart.pk">
-                                support@apricart.pk
-                            </a>
-                        </p>
-                    </div>
-                </div>
-
-                <div className="footer-right">
-                    <p className="footer-company-about">
-                        <span>Cities</span>
-                        Karachi
-                        <br />
-                        Peshawar
-                    </p>
-
-                    <div className="footer-icons">
-                        <a href="https://www.facebook.com/apricartonlinegrocery/">
-                            <i className="fab fa-facebook"></i>
-                        </a>
-                        <a href="https://twitter.com/apricartpk?lang=en">
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                        <a href="https://pk.linkedin.com/company/apricartestores">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                        <a href="#">
-                            <i className="fab fa-github"></i>
-                        </a>
-                    </div>
-                    <div className="copytext">
-                        <h3>Install Our App</h3>
-                        <ul>
-                            <li>
-                                <a href="#">
-                                    <img
-                                        src="/assets/images/playstore-img.png"
-                                        // width={80} height={40} margin-right={50}
-                                        className="img-fluid"
-                                        alt=""
-                                    />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img
-                                        src="/assets/images/appstore-img.png"
-                                        // width={80} height={40}
-                                        className="img-fluid king"
-                                        alt=""
-                                    />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    {/* <!-- <p class="footer-company-name">Apricart E-Stores Pvt Ltd    <br>  All rights reserved</p> --> */}
-                </div>
-            </footer>
-        </section>
+</div>        
     );
 }
