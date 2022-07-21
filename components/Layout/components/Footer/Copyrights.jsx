9ce9825e88f9fee80a44982ba44a6313fbcaf204
Syed Hassam Kazmi <59208947+Hassamkazmi@users.noreply.github.com>
2022-07-21 12:27:39 +0500

    All

--- components/Layout/components/Footer/Copyrights.jsx
+++ components/Layout/components/Footer/Copyrights.jsx
@@ -1,9 +1,10 @@
 export default function Layout() {
     return (
-        <section className="copyright-sec">
-            <div className="copyright">
-                Apricart E-Stores Pvt Ltd All Rights Reserved
+        
+     <section className="relative inset-x-0 bottom-0 ">
+            <div className="flex bg-white justify-center items-center h-[50px]">
+               <p className="font-bold">Apricart E-Stores Pvt Ltd All Rights Reserved</p> 
             </div>
-        </section>
+    </section>
     )
 }
