842af2e62d390e7dcd8a0f0afe01b14f7a629446
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-07-25 13:46:41 +0500

    banner sizes fixed

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
-import Image from "next/image";
-import Categories from "../components/Layout/components/Categories/Categories";
-import Cookies from 'universal-cookie';
-import { useEffect, useState } from "react";
-import { base_url_api } from '../information.json'
-import axios from "axios";
-import ScrollingProducts from "../components/Layout/components/Products/ScrollingProducts";
-import { getGeneralApiParams } from '../helpers/ApiHelpers'
-import Banner from "../components/Layout/components/Banner/Banner";
-import MainProducts from '../components/Layout/components/Products/MainProducts'
-import Link from "next/link";
-import { useDispatch } from "react-redux";
-import { updateTicker } from "../redux/general.slice";
-import HeadTag from "../components/Layout/components/Head/HeadTag";
-import storeBackgroundImage from '../public/assets/images/storeBackground.png'
-import lifestyle from '../public/assets/images/banners/lifestyle.jpeg'
-import nationals from '../public/assets/images/banners/nationals.jpeg'
-import mainBanner from '../public/assets/images/banners/mainBanner.png'
+import Image from "next/image"
+import Categories from "../components/Layout/components/Categories/Categories"
+import Cookies from "universal-cookie"
+import { useEffect, useState } from "react"
+import { base_url_api } from "../information.json"
+import axios from "axios"
+import ScrollingProducts from "../components/Layout/components/Products/ScrollingProducts"
+import { getGeneralApiParams } from "../helpers/ApiHelpers"
+import Banner from "../components/Layout/components/Banner/Banner"
+import MainProducts from "../components/Layout/components/Products/MainProducts"
+import Link from "next/link"
+import { useDispatch } from "react-redux"
+import { updateTicker } from "../redux/general.slice"
+import HeadTag from "../components/Layout/components/Head/HeadTag"
+import storeBackgroundImage from "../public/assets/images/storeBackground.png"
+import lifestyle from "../public/assets/images/banners/lifestyle.jpeg"
+import nationals from "../public/assets/images/banners/nationals.jpeg"
+import mainBanner from "../public/assets/images/banners/mainBanner.png"
 
 export default function Home() {
-	const cookies = new Cookies();
-	const dispatch = useDispatch();
-	let token = cookies.get('cookies-token')
+	const cookies = new Cookies()
+	const dispatch = useDispatch()
+	let token = cookies.get("cookies-token")
 
 	const [categories, setCategories] = useState(null)
 	const [homeData, setHomeData] = useState(null)
-	const [errorMessage, setErrorMessage] = useState('Loading');
+	const [errorMessage, setErrorMessage] = useState("Loading")
 
 	useEffect(() => {
 		getHomeDataApi()
@@ -32,19 +32,27 @@ export default function Home() {
 	}, [])
 
 	const getHomeDataApi = async () => {
-		let { city, latitude, longitude, userId, headers } = getGeneralApiParams()
+		let { city, latitude, longitude, userId, headers } =
+			getGeneralApiParams()
 
-		let url = base_url_api + '/home/all?client_lat=' + latitude + '&client_long=' + longitude + '&city=' + city + '&lang=en&userid=' + userId + '&web=false&client_type=apricart&prod_type=cus&order_type=delivery'
+		let url =
+			base_url_api +
+			"/home/all?client_lat=" +
+			latitude +
+			"&client_long=" +
+			longitude +
+			"&city=" +
+			city +
+			"&lang=en&userid=" +
+			userId +
+			"&web=false&client_type=apricart&prod_type=cus&order_type=delivery"
 		try {
-			let response = await axios.get(
-				url,
-				{
-					'headers': headers
-				}
-			)
+			let response = await axios.get(url, {
+				headers: headers,
+			})
 
-			setHomeData(response.data.data);
-			dispatch(updateTicker(response.data.data.ticker));
+			setHomeData(response.data.data)
+			dispatch(updateTicker(response.data.data.ticker))
 		} catch (error) {
 			setErrorMessage(error.message)
 		}
@@ -52,14 +60,15 @@ export default function Home() {
 
 	const getCategoriesApi = async () => {
 		let { city, headers } = getGeneralApiParams()
-		let url = base_url_api + '/catalog/categories?level=all&client_type=apricart&city=' + city
+		let url =
+			base_url_api +
+			"/catalog/categories?level=all&client_type=apricart&city=" +
+			city
 
 		try {
-			let response = await axios.get(url,
-				{
-					headers: headers
-				}
-			)
+			let response = await axios.get(url, {
+				headers: headers,
+			})
 			setCategories(response.data.data)
 		} catch (error) {
 			console.log(error)
@@ -69,24 +78,22 @@ export default function Home() {
 	if (!homeData) {
 		return (
 			<div>
-				<p>
-					{errorMessage}
-				</p>
+				<p>{errorMessage}</p>
 			</div>
 		)
 	}
 
 	return (
 		<div className="space-y-8">
-			<HeadTag title={'APRICART'} />
+			<HeadTag title={"APRICART"} />
 			{/* BANNERS SECTION hidden on phone */}
 			<section className="hidden relative w-screen aspect-[16/6] lg:grid grid-cols-12 items-center">
 				{/* BACKGROUND IMAGE */}
 				<div className="absolute w-full h-full blur-lg">
 					<Image
 						src={storeBackgroundImage}
-						layout={'responsive'}
-						alt='banner'
+						layout={"responsive"}
+						alt="banner"
 					/>
 				</div>
 				{/* SCROLLING BANNER */}
@@ -94,8 +101,8 @@ export default function Home() {
 					<div className="w-full">
 						<Image
 							src={mainBanner}
-							layout={'responsive'}
-							alt='banner'
+							layout={"responsive"}
+							alt="banner"
 						/>
 					</div>
 					{/* <Banner
@@ -104,19 +111,11 @@ export default function Home() {
 				</section>
 				{/* STATIC BANNERS */}
 				<section className="col-span-5 grid grid-rows-2 h-full w-full justify-start items-center space-y-8">
-					<div className="relative w-[500px] h-[200px]">
-						<Image
-							src={nationals}
-							layout={'fill'}
-							alt='banner'
-						/>
+					<div className="relative w-[500px] h-[180px] 2xl:w-[700px] 2xl:h-[280px]">
+						<Image src={nationals} layout={"fill"} alt="banner" />
 					</div>
-					<div className="relative w-[500px] h-[200px]">
-						<Image
-							src={lifestyle}
-							layout={'fill'}
-							alt='banner'
-						/>
+					<div className="relative w-[500px] h-[180px] 2xl:w-[700px] 2xl:h-[280px]">
+						<Image src={lifestyle} layout={"fill"} alt="banner" />
 					</div>
 				</section>
 				{/* {homeData.banners.length >= 2 && (
@@ -142,16 +141,16 @@ export default function Home() {
 				<div className="absolute w-full h-full blur-lg">
 					<Image
 						src={storeBackgroundImage}
-						layout={'responsive'}
-						alt=''
+						layout={"responsive"}
+						alt=""
 					/>
 				</div>
 				{/* MAIN BANNER */}
 				<section className="w-full">
 					<Image
 						src={mainBanner}
-						layout={'responsive'}
-						alt='banner'
+						layout={"responsive"}
+						alt="banner"
 					/>
 				</section>
 				{/* {homeData.banners.length >= 2 && (
@@ -174,11 +173,7 @@ export default function Home() {
 			<div className="grid grid-cols-5 gap-12">
 				{/* CATEGORIES SECTION */}
 				<section className="hidden lg:col-span-1 lg:block">
-					{categories && (
-						<Categories
-							categories={categories}
-						/>
-					)}
+					{categories && <Categories categories={categories} />}
 				</section>
 				{/* PRODUCTS SECTION */}
 				<section className="col-span-5 lg:col-span-4 space-y-12">
@@ -192,11 +187,10 @@ export default function Home() {
 						let { name, offerId } = product
 
 						// If the name is 'Upload Grocery List', we have to return a button which allows to upload grocery list
-						if (name === 'Upload Grocery List') {
+						if (name === "Upload Grocery List") {
 							return (
 								<div className="mb-2 mt-8 flex flex-row w-full rounded-xl p-2 bg-main-yellow items-center align-center justify-around">
-									<Link href={'/grocery_list'} passHref
-									>
+									<Link href={"/grocery_list"} passHref>
 										<a className="text-main-blue font-bold text-lg w-full text-center">
 											UPLOAD your GROCERY LIST
 										</a>
@@ -213,8 +207,8 @@ export default function Home() {
 										<section className="w-full">
 											<Image
 												src={lifestyle}
-												layout={'responsive'}
-												alt=''
+												layout={"responsive"}
+												alt=""
 											/>
 										</section>
 									</section>
@@ -223,22 +217,18 @@ export default function Home() {
 										<section className="w-full">
 											<Image
 												src={nationals}
-												layout={'responsive'}
-												alt=''
+												layout={"responsive"}
+												alt=""
 											/>
 										</section>
 									</section>
 								)}
-								<MainProducts
-									key={offerId}
-									section={product}
-								/>
+								<MainProducts key={offerId} section={product} />
 							</section>
 						)
 					})}
 				</section>
 			</div>
-
 		</div>
-	);
+	)
 }
