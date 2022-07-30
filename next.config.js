6e20c0598405e2849035aa001c142607ae24fd0a
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-07-30 17:01:02 +0500

    init

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ module.exports = {
   images: {
     domains: ['cbe.apricart.pk', '15.184.248.248', 'staging.apricart.pk']
   },
+  async redirects() {
+		return [
+			{
+				source: "/index.html",
+				destination: "/",
+				permanent: true,
+			},
+		]
+	},
   basePath: '/web'
 }
 
