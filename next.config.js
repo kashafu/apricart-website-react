64c02d7292d907c9947afdebac0f9a82adb8bead
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-07-30 16:51:44 +0500

    added redircet from index.html

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['cbe.apricart.pk', '15.184.248.248', 'staging.apricart.pk']
-  },
-  // basePath: '/web'
+	reactStrictMode: true,
+	images: {
+		domains: ["cbe.apricart.pk", "15.184.248.248", "staging.apricart.pk"],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/index.html",
+				destination: "/",
+				permanent: true,
+			},
+		]
+	},
+	// basePath: '/web'
 }
 
 // module.exports = nextConfig
