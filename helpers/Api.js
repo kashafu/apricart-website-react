1728fb8aec81011212ef6ebfc544eb965bc36733
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-08-02 11:22:48 +0500

    made get categories api

--- helpers/Api.js
+++ helpers/Api.js
@@ -1,33 +1,41 @@
 import axios from "axios"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { base_url_api } from "../information.json"
 import { getGeneralApiParams } from "./ApiHelpers"
 
-export const useTestApi = async () => {
+export const useTestApi = () => {
 	const [isLoading, setIsLoading] = useState(true)
 	const [data, setData] = useState(null)
-    const [response, setResponse] = useState(null)
-    const [errorResponse, setErrorResponse] = useState(null)
-    const [errorMessage, setErrorMessage] = useState('')
+	const [response, setResponse] = useState(null)
+	const [errorResponse, setErrorResponse] = useState(null)
+	const [errorMessage, setErrorMessage] = useState("")
 
-	let { city, headers, userId } = getGeneralApiParams()
-	let url =
-		base_url_api +
-		"/catalog/categories?level=all&client_type=apricart&city=" +
-		city +
-		"&userid=" +
-		userId
+	useEffect(()=>{
+		callApi()
+	}, [])
 
-	try {
-		setResponse(await axios.get(url, {
-			headers: headers,
-		}))
-		setData(response.data.data)
-	} catch (error) {
-		setErrorResponse(error?.response)
-        setErrorMessage(error?.response?.data?.message)
+	const callApi = async () => {
+		let { city, headers, userId } = getGeneralApiParams()
+		let url =
+			base_url_api +
+			"/catalog/categories?level=all&client_type=apricart&city=" +
+			city +
+			"&userid=" +
+			userId
+	
+		try {
+			setResponse(
+				await axios.get(url, {
+					headers: headers,
+				})
+			)
+			setData(response.data.data)
+		} catch (error) {
+			setErrorResponse(error?.response)
+			setErrorMessage(error?.response?.data?.message)
+		}
+		setIsLoading(false)
 	}
-    setIsLoading(false)
 
 	return { isLoading, data, errorMessage, response, errorResponse }
 }
