341faf3df68d739286fe08a276575b0a80f53bf9
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-08-03 14:45:24 +0500

    added home api to api folder

--- helpers/Api.js
+++ helpers/Api.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react"
 import { base_url_api } from "../information.json"
 import { getGeneralApiParams } from "./ApiHelpers"
 
-export const useTestApi = () => {
+export const useCategoriesApi = () => {
 	const [isLoading, setIsLoading] = useState(true)
 	const [data, setData] = useState(null)
 	const [response, setResponse] = useState(null)
@@ -38,4 +38,63 @@ export const useTestApi = () => {
 	}
 
 	return { isLoading, data, errorMessage, response, errorResponse }
+}
+
+export const useHomeApi = () => {
+	const [isLoading, setIsLoading] = useState(true)
+	const [data, setData] = useState(null)
+    const [showPopupAd, setShowPopupAd] = useState(false)
+	const [response, setResponse] = useState(null)
+	const [errorResponse, setErrorResponse] = useState(null)
+	const [errorMessage, setErrorMessage] = useState("")
+
+	useEffect(()=>{
+		callApi()
+	}, [])
+
+	const callApi = async () => {
+		let {
+			city,
+			latitude,
+			longitude,
+			userId,
+			headers,
+			prodType,
+			clientType,
+			orderType,
+		} = getGeneralApiParams()
+
+		let url =
+			base_url_api +
+			"/home/all?client_lat=" +
+			latitude +
+			"&client_long=" +
+			longitude +
+			"&city=" +
+			city +
+			"&lang=en&userid=" +
+			userId +
+			"&web=true&client_type=" +
+			clientType +
+			"&prod_type=" +
+			prodType +
+			"&order_type=" +
+			orderType
+	
+		try {
+			setResponse(
+				await axios.get(url, {
+					headers: headers,
+				})
+			)
+			setData(response.data.data)
+            setShowPopupAd(response.data.data.dialog)
+		} catch (error) {
+			setErrorResponse(error?.response)
+			setErrorMessage(error?.response?.data?.message)
+		}
+		setIsLoading(false)
+	}
+
+	return { isLoading, data, errorMessage, response, errorResponse, showPopupAd }
 }
