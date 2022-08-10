dfa6a2910db266d72b6e8e59b8220fd6968bd1f7
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-08-10 10:56:58 +0500

    added banners

--- components/Layout/components/Banner/Banner.js
+++ components/Layout/components/Banner/Banner.js
@@ -1,41 +1,36 @@
+import Image from "next/image";
 import Link from "next/link";
-import { Carousel } from "react-responsive-carousel"
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-export default function Banner({ banners }) {
-    if (!banners) {
-        return (
-            <div>
-                <p>
-                    Loading Banners
-                </p>
-            </div>
-        )
-    }
-
+export default function Banner({ bannerImage, offerId }) {
     return (
-        <div className="scale-y-75 md:scale-100">
-            <Carousel autoPlay={true} infiniteLoop={true} swipeable={true} showArrows={true} interval={5000}>
-                {banners.map((banner) => {
-                    let { id, bannerUrlWeb } = banner
-                    return (
-                        <Link href="/offers/[id]"
-                            as={
-                                "/offers/" + id
-                            }
-                            passHref
-                            key={id}
-                        >
-                            <button className="carousel-inner">
-                                <img
-                                    src={bannerUrlWeb}
-                                    alt=""
-                                />
-                            </button>
-                        </Link>
-                    )
-                })}
-            </Carousel>
+        <div className="w-full rounded-xl overflow-hidden">
+            {offerId == 0 ? (
+                <Image
+                    src={bannerImage}
+                    layout={'responsive'}
+                    alt={"banner image"}
+                    width={'100%'}
+                    height={20}
+                />
+            ) : (
+                <Link href="/offers/[id]"
+                    as={
+                        "/offers/" + offerId
+                    }
+                    passHref
+                    key={offerId}
+                >
+                    <a className="w-full">
+                        <Image
+                            src={bannerImage}
+                            layout={'responsive'}
+                            alt={"banner image"}
+                            width={'100%'}
+                            height={20}
+                        />
+                    </a>
+                </Link>
+            )}
         </div>
     )
 }
