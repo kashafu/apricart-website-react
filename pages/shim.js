a63ccf1e810889d472b353f599ad5014f728f390
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-08-15 16:20:49 +0500

    cleaned shim

--- pages/shim.js
+++ pages/shim.js
@@ -1,305 +1,80 @@
-export default function SingleProductshimmer(){
-    const [showAddToCart, setShowAddToCart] = useState(false)
-
-	let divStyle = showAddToCart
-		? "drop-shadow-xl z-10"
-		: "border-r-2 border-b-2"
-    return(
-        <>
-        <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 sm:grid-rows-2 w-full">
-       
- <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-                    <div className={"animate-pulse h-[400px] m-2 rounded-lg w-full " +
-					[divStyle] }>
-                        <div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg">
-
-                        </div>
-                        <div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50">
-
-</div>
-                        <div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50">
-
-                        </div>
-
-                        
-
-
-                  
-
-
-                  
-                    </div>
-
-<br/>
-
-
-
-                 
-
-
-      
-                    
-
-
-
-
-
-                    </div>
-        </>
-    )
+export default function SingleProductshimmer() {
+	return (
+		<div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 sm:grid-rows-2 w-full">
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+			<div className="animate-pulse h-[400px] m-2 rounded-lg w-full">
+				<div className="animate-pulse w-full h-2/3 bg-slate-50 rounded-lg"></div>
+				<div className="animate-pulse w-full my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-2 h-[30px] rounded-lg bg-slate-50"></div>
+				<div className="animate-pulse w-3/5 my-1 h-[40px] rounded-lg bg-slate-50"></div>
+			</div>
+
+			<br />
+		</div>
+	)
 }
