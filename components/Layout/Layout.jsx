e102d5fc4de0017d8ea3b012062d69a84aaf7c98
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-08-17 13:01:54 +0500

    added select address warning

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,19 +1,27 @@
-import React, { createContext, useState } from "react";
+import { createContext, useState } from "react";
 import TopBar from "./components/TopBar/TopBar";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import Copyrights from "./components/Footer/Copyrights";
 import { useRouter } from "next/router";
-import TypeCardSelector from "./components/Cards/TypeCardSelector";
 import FooterSleek from "./components/Footer/FooterSleek";
+import { getGeneralApiParams } from "../../helpers/ApiHelpers";
+import { toast } from 'react-toastify'
 export const AppContext = createContext();
 
 export default function Layout(props) {
 	const router = useRouter();
 	const [appState, setAppState] = useState({ sideDrawerStatus: false });
+	const { token, selectedAddress } = getGeneralApiParams()
+
 	const handleAppState = (newState) => {
 		setAppState({ ...appState, ...newState });
 	};
+
+	if (token && !selectedAddress) {
+		toast.warn("Please select your delivery address")
+		console.log("Please select your delivery address")
+	}
+
 	return (
 		<AppContext.Provider value={{ appState, handleAppState }}>
 			<div className="flex flex-col min-h-screen max-w-screen">
@@ -22,11 +30,11 @@ export default function Layout(props) {
 					router.pathname === "/terms-of-use-mobile" ||
 					router.pathname === "/faqs-mobile"
 				) && (
-					<div>
-						<TopBar />
-						<Header />
-					</div>
-				)}
+						<div>
+							<TopBar />
+							<Header />
+						</div>
+					)}
 				<div className="flex-1 min-h-full min-w-full pt-2 pb-10 px-2 md:px-8 bg-white">
 					<div className="pt-[3rem] lg:pt-[5rem]">{props.children}</div>
 				</div>
@@ -35,11 +43,11 @@ export default function Layout(props) {
 					router.pathname === "/terms-of-use-mobile" ||
 					router.pathname === "/faqs-mobile"
 				) && (
-					<div className="mt-auto">
-						<FooterSleek />
-						<Copyrights />
-					</div>
-				)}
+						<div className="mt-auto">
+							<FooterSleek />
+							<Copyrights />
+						</div>
+					)}
 			</div>
 		</AppContext.Provider>
 	);
