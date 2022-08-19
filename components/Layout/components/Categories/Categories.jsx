08c701ad6843a355807517ba6f48c189134c0d7d
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-08-19 13:24:03 +0500

    categries sticky

--- components/Layout/components/Categories/Categories.jsx
+++ components/Layout/components/Categories/Categories.jsx
@@ -27,18 +27,81 @@ export default function Categories() {
 	}
 
 	return (
-		<div className="space-y-4">
-			<p className="text-main-blue font-bold lg:text-2xl 2xl:text-3xl">
-				- CATEGORIES
-			</p>
-			<div className="grid grid-flow-row divide-y">
-				{categories.map((category) => {
-					let { id, name, childrenData } = category
-					return (
-						<div key={id} className="w-full">
-							{isSelected == id ? (
-								<div className="grid grid-flow-row">
-									<div className="grid grid-cols-5 py-2 pl-4 bg-main-blue items-center rounded-t-lg">
+		<div className="sticky top-[100px] w-full">
+			<div className="space-y-4">
+				<p className="text-main-blue font-bold lg:text-2xl 2xl:text-3xl">
+					CATEGORIES
+				</p>
+				<div className="grid grid-flow-row divide-y">
+					{categories.map((category) => {
+						let { id, name, childrenData } = category
+						return (
+							<div key={id} className="w-full">
+								{isSelected == id ? (
+									<div className="grid grid-flow-row">
+										<div className="grid grid-cols-5 py-2 pl-4 bg-main-blue items-center rounded-t-lg">
+											<Link
+												href={
+													"/category/" +
+													toKebabCase(name) +
+													"/" +
+													id
+												}
+												passHref
+											>
+												<a className="text-white font-bold col-span-4">
+													{name}
+												</a>
+											</Link>
+											<button
+												onClick={() => {
+													if (isSelected == id) {
+														setIsSelected("")
+													} else {
+														setIsSelected(id)
+													}
+												}}
+											>
+												<Image
+													src={
+														isSelected == id
+															? upArrowIcon
+															: downArrowIcon
+													}
+													width={20}
+													height={20}
+													alt="icon"
+												/>
+											</button>
+										</div>
+										<div className="bg-main-blue rounded-b-lg px-6 divide-y animate-dropdown">
+											{childrenData.map((subCategory) => {
+												let { id, name } = subCategory
+												return (
+													<div
+														key={id}
+														className="flex flex-row w-full items-center py-2"
+													>
+														<Link
+															href={
+																"/category/" +
+																toKebabCase(name) +
+																"/" +
+																id
+															}
+															passHref
+														>
+															<a className="font-bold text-main-yellow">
+																{name}
+															</a>
+														</Link>
+													</div>
+												)
+											})}
+										</div>
+									</div>
+								) : (
+									<div className="grid grid-cols-5 py-2 pl-4 bg-white items-center rounded-lg">
 										<Link
 											href={
 												"/category/" +
@@ -48,17 +111,14 @@ export default function Categories() {
 											}
 											passHref
 										>
-											<a className="text-white font-bold col-span-4">
+											<a className="text-main-blue col-span-4">
 												{name}
 											</a>
 										</Link>
+
 										<button
 											onClick={() => {
-												if (isSelected == id) {
-													setIsSelected("")
-												} else {
-													setIsSelected(id)
-												}
+												setIsSelected(id)
 											}}
 										>
 											<Image
@@ -69,73 +129,15 @@ export default function Categories() {
 												}
 												width={20}
 												height={20}
-												alt="icon"
+												alt={"icon"}
 											/>
 										</button>
 									</div>
-									<div className="bg-main-blue rounded-b-lg px-6 divide-y animate-dropdown">
-										{childrenData.map((subCategory) => {
-											let { id, name } = subCategory
-											return (
-												<div
-													key={id}
-													className="flex flex-row w-full items-center py-2"
-												>
-													<Link
-														href={
-															"/category/" +
-															toKebabCase(name) +
-															"/" +
-															id
-														}
-														passHref
-													>
-														<a className="font-bold text-main-yellow">
-															{name}
-														</a>
-													</Link>
-												</div>
-											)
-										})}
-									</div>
-								</div>
-							) : (
-								<div className="grid grid-cols-5 py-2 pl-4 bg-white items-center rounded-lg">
-									<Link
-										href={
-											"/category/" +
-											toKebabCase(name) +
-											"/" +
-											id
-										}
-										passHref
-									>
-										<a className="text-main-blue col-span-4">
-											{name}
-										</a>
-									</Link>
-
-									<button
-										onClick={() => {
-											setIsSelected(id)
-										}}
-									>
-										<Image
-											src={
-												isSelected == id
-													? upArrowIcon
-													: downArrowIcon
-											}
-											width={20}
-											height={20}
-											alt={"icon"}
-										/>
-									</button>
-								</div>
-							)}
-						</div>
-					)
-				})}
+								)}
+							</div>
+						)
+					})}
+				</div>
 			</div>
 		</div>
 	)
