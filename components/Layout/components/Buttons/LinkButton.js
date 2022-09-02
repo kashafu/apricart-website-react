67a87adfda7c4dffa4fcb52b556cd378a18a628e
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-09-02 10:46:32 +0500

    added icons prop to link button

--- components/Layout/components/Buttons/LinkButton.js
+++ components/Layout/components/Buttons/LinkButton.js
@@ -1,6 +1,7 @@
+import Image from "next/image"
 import Link from "next/link"
 
-export default function LinkButton({ text, path, onClick }) {
+export default function LinkButton({ text, path, onClick, icon, height, width }) {
     return (
         <Link href={path} passHref>
             <div className="w-full bg-main-yellow rounded-2xl text-center p-2 font-bold text-main-blue"
@@ -10,6 +11,14 @@ export default function LinkButton({ text, path, onClick }) {
                     }
                 }}
             >
+                {icon && (
+                    <Image
+                        src={icon}
+                        width={width}
+                        height={height}
+                        alt='icon'
+                    />
+                )}
                 <a className="font-lato">
                     {text}
                 </a>
