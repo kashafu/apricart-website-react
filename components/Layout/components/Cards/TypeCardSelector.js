b5815a8ffb0d0139baeb0787bfc942a0ae474876
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-09-07 10:54:39 +0500

    updated typecardselector

--- components/Layout/components/Cards/TypeCardSelector.js
+++ components/Layout/components/Cards/TypeCardSelector.js
@@ -21,7 +21,7 @@ const TypeCardSelector = () => {
     }, [citySelector])
 
     return (
-        <div className="grid grid-cols-3 gap-x-2 lg:gap-x-16 lg:px-16 bg-white">
+        <div className="grid grid-cols-3 gap-x-2 lg:gap-x-8 2xl:gap-x-16 lg:px-8 2xl:px-16 bg-white">
             <HomeDeliveryCard isDisabled={isDisabled} />
             <ClickAndCollectCard isDisabled={isDisabled} />
             <BulkBuyCard />
