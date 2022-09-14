32e9f9efe260e5b915421340eeb8bacb471c73e1
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-09-14 17:05:48 +0500

    changec font size of js popup

--- pages/checkout.js
+++ pages/checkout.js
@@ -436,7 +436,7 @@ export default function Checkout() {
 			<div className="animate-dropdown fixed inset-0 h-full w-full backdrop-blur-sm z-50">
 				<div className="fixed w-3/4 lg:w-1/3 h-1/3 bg-white border-2 shadow-2xl inset-0 m-auto z-50 rounded-lg p-2">
 					<div className="flex flex-col justify-between h-full w-full">
-						<p className="font-nunito text-black text-lg text-center">
+						<p className="font-nunito text-black text-xl text-center">
 							Your Zindagi User ID and number is needed to proceed. Please confirm to grant access to this information.
 						</p>
 						<div className="flex flex-row space-x-4">
