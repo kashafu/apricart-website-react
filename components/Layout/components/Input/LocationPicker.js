05e8d7ccd764658808d3ed0fd81da5539ee0ce03
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-09-16 13:50:28 +0500

    temp

--- components/Layout/components/Input/LocationPicker.js
+++ components/Layout/components/Input/LocationPicker.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-// import PlacesAutocomplete from './PlacesAutocomplete';
+import { GoogleMap, LoadScript, Marker, Autocomplete } from '@react-google-maps/api';
+// import Autocomplete from "react-google-autocomplete";
+
 let defaultLocation = {
     lat: 24.917122827062762,
     lng: 67.09610049861793
@@ -30,23 +31,29 @@ export default function LocationPicker({ label, onChangeLatitude, onChangeLongit
     }
 
     return (
-        <LoadScript
-            googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
-        >
-            {/* <PlacesAutocomplete /> */}
-            <GoogleMap
-                // center={startingLocation.lat != '' && startingLocation.lng != '' ? startingLocation : defaultLocation}
-                center={defaultLocation}
-                zoom={15}
-                onLoad={handleOnLoad}
-                onCenterChanged={handleCenterChanged}
-                mapContainerStyle={{ width: '100%', height: '100%' }}
-            >
-                <Marker
-                    position={centerLocation}
-                    draggable={false}
-                />
-            </GoogleMap>
-        </LoadScript>
+        // <Autocomplete
+        //     apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+        //     onPlaceSelected={(place) => console.log(place)}
+        // />
+        <Autocomplete
+
+        />
     )
+    {/* <LoadScript
+        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+    >
+        <GoogleMap
+            // center={startingLocation.lat != '' && startingLocation.lng != '' ? startingLocation : defaultLocation}
+            center={defaultLocation}
+            zoom={15}
+            onLoad={handleOnLoad}
+            onCenterChanged={handleCenterChanged}
+            mapContainerStyle={{ width: '100%', height: '100%' }}
+        >
+            <Marker
+                position={centerLocation}
+                draggable={false}
+            />
+        </GoogleMap>
+    </LoadScript> */}
 }
