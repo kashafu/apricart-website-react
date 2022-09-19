f3b29f6593e2fa60110ad48e73dc7668097e4043
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-09-19 12:10:27 +0500

    updated banners

--- components/Layout/components/Banner/Carousel.jsx
+++ components/Layout/components/Banner/Carousel.jsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 import { useSelector } from 'react-redux'
 
 import karachiMainBanner from '../../../../public/assets/images/banners/mainBanner.png'
-import karachiBulkBanner from '../../../../public/assets/images/banners/karachiBulkBanner.png'
+import karachiBulkBanner from '../../../../public/assets/images/banners/peshawarMainBanner.jpeg'
 import peshawarMainBanner from '../../../../public/assets/images/banners/peshawarMainBanner.jpeg'
-import karachiScrollingBanner1 from '../../../../public/assets/images/banners/blessedFridayKarachiHomeDelivery.png'
+// import karachiScrollingBanner1 from '../../../../public/assets/images/banners/blessedFridayKarachiHomeDelivery.png'
 // import karachiScrollingBanner2 from '../../../../public/assets/images/banners/everythingIn75.png'
 
 const Carousel = () => {
@@ -40,7 +40,7 @@ const Carousel = () => {
                         </Slider>
                     ) : (
                         <Slider {...settings}>
-                            <div className='w-full h-full'>
+                            {/* <div className='w-full h-full'>
                                 <Link href={'/'} passHref>
                                     <a className="w-full h-full">
                                         <Image
@@ -50,7 +50,7 @@ const Carousel = () => {
                                         />
                                     </a>
                                 </Link>
-                            </div>
+                            </div> */}
                             {/* <div className='w-full h-full'>
                                 <Link href={'/category/just-rs.75/1242'} passHref>
                                     <a className="w-full">
