9f7e069727ece371a00507c5f7631cb53aa6e596
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-10-01 07:45:37 +0500

    feedback page init

---
+++ pages/feedback.js
@@ -0,0 +1,9 @@
+const FeedbackPage = () => {
+    return (
+        <div className="flex w-full h-full items-stretch bg-black">
+            <p>AddAddressCard</p>
+        </div>
+    )
+}
+
+export default FeedbackPage
