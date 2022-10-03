ac2706ff93bb70ec844b7de944341b4e184bb214
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-10-03 13:11:47 +0500

    list products made

--- components/Layout/components/Products/ListProducts.js
+++ components/Layout/components/Products/ListProducts.js
@@ -6,31 +6,8 @@ export default function ListProducts({ products }) {
 	}
 
 	return (
-		<section className="space-y-4 px-2">
-			{/* MOBILE VIEW PRODUCTS */}
-			<section className="grid grid-cols-2 sm:grid-cols-3 md:hidden gap-2">
-				{products.map((product) => {
-					let { id } = product
-					return (
-						<div key={id}>
-							<SingleProduct product={product} />
-						</div>
-					)
-				})}
-			</section>
-			{/* LAPTOP VIEW PRODUCTS */}
-			<section className="hidden md:grid md:grid-cols-4 gap-2 xl:hidden">
-				{products.map((product) => {
-					let { id } = product
-					return (
-						<div key={id}>
-							<SingleProduct product={product} />
-						</div>
-					)
-				})}
-			</section>
-			{/* DESKTOP VIEW PRODUCTS */}
-			<section className="hidden xl:grid xl:grid-cols-5 3xl:grid-cols-6 gap-2">
+		<section className="px-2">
+			<section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 3xl:grid-cols-6 gap-2">
 				{products.map((product) => {
 					let { id } = product
 					return (
