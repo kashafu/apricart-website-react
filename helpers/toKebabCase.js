0b5761bfd98c85c1f795730782cabc4a8fcd2c00
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-10-06 17:14:20 +0500

    added uri encoding to urls

--- helpers/toKebabCase.js
+++ helpers/toKebabCase.js
@@ -1,15 +1,15 @@
 export default function toKebabCase(word) {
-	return word
+	return encodeURI(word
 		.replace(/([a-z])([A-Z])/g, "$1-$2")
 		.replace(/[\s_]+/g, "-")
-		.toLowerCase()
+		.toLowerCase())
 }
 
 export function fromKebabCase(word) {
 	let temp = word.split('-')
 	let out = ''
-	temp.forEach((item)=>{
+	temp.forEach((item) => {
 		out = out.concat(item + " ")
 	})
-	return out
+	return decodeURI(out)
 }
