175f07cf3aa8d10ab81fda23cd517dd52f97c317
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-10-07 16:16:30 +0500

    list products key sku instead of id

--- components/Layout/components/Products/ListProducts.js
+++ components/Layout/components/Products/ListProducts.js
@@ -9,9 +9,9 @@ export default function ListProducts({ products }) {
 		<section className="px-2">
 			<section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 3xl:grid-cols-6 gap-2">
 				{products.map((product) => {
-					let { id } = product
+					let { sku } = product
 					return (
-						<div key={id}>
+						<div key={sku}>
 							<SingleProduct product={product} />
 						</div>
 					)
