b700489e3dbe75191a436f016341bbe8656f6f34
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-10-07 16:16:15 +0500

    cleaned search products

--- pages/products/search/[term].js
+++ pages/products/search/[term].js
@@ -1,29 +1,51 @@
 import { useRouter } from "next/router"
+
+import ListProductsShimmer from "../../../components/Layout/components/Loaders/Shimmers/ListProductsShimmer"
 import HeadTag from "../../../components/Layout/components/Head/HeadTag"
-import Categories from "../../../components/Layout/components/Categories/Categories"
-import SingleProduct from "../../../components/Layout/components/Products/SingleProduct"
 import PageHeading from '../../../components/Layout/components/Typography/PageHeading'
 import { useSearchResultsApi } from "../../../helpers/Api"
+import ListProducts from "../../../components/Layout/components/Products/ListProducts"
+import CategoryAndItemsLayout from "../../../components/Layout/components/Layouts/CategoryAndItemsLayout"
 
 const SearchResults = () => {
     const router = useRouter()
     const { term } = router.query
-    const { isLoading, searchResults, errorMessage, response } = useSearchResultsApi()
 
-    if (isLoading) {
+    const Products = () => {
+        const { isLoading, searchResults, errorMessage } = useSearchResultsApi()
+
+        if (isLoading) {
+            return (
+                <ListProductsShimmer />
+            )
+        }
+
+        if (!searchResults) {
+            return (
+                <p>{errorMessage}</p>
+            )
+        }
+
+        if (searchResults.length === 0) {
+            return (
+                <p>No items match the search criteria</p>
+            )
+        }
+
         return (
-            <div>
-                <HeadTag title={"Loading Search Results"} />
-                <p>Loading</p>
-            </div>
+            <ListProducts
+                products={searchResults}
+            />
         )
     }
 
-    if (!searchResults) {
+    const PageItems = () => {
         return (
-            <div>
-                <HeadTag title={"Search"} />
-                <p>{errorMessage}</p>
+            <div className='space-y-4'>
+                <PageHeading
+                    text={'SEARCH RESULTS FOR: ' + term}
+                />
+                <Products />
             </div>
         )
     }
@@ -31,32 +53,9 @@ const SearchResults = () => {
     return (
         <div>
             <HeadTag title={term} />
-            <div className="grid grid-cols-5 gap-8">
-                {/* CATEGORIES SECTION */}
-                <section className="hidden lg:col-span-1 lg:block">
-                    <Categories />
-                </section>
-                {/* PRODUCTS SECTION */}
-                <section className="col-span-5 lg:col-span-4 space-y-12">
-                    <PageHeading
-                        text={'SEARCH RESULTS FOR: ' + term}
-                    />
-                    {searchResults.length == 0 ? (
-                        <div>NO ITEMS EXIST</div>
-                    ) : (
-                        <section className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
-                            {searchResults.map((result) => {
-                                let { sku } = result
-                                return (
-                                    <div key={sku}>
-                                        <SingleProduct product={result} />
-                                    </div>
-                                )
-                            })}
-                        </section>
-                    )}
-                </section>
-            </div>
+            <CategoryAndItemsLayout>
+                <PageItems />
+            </CategoryAndItemsLayout>
         </div>
     )
 }
