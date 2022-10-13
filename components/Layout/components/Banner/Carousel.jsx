343bd9a76e5cd1ff3aa728693428a712f90210c9
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-10-13 15:52:18 +0500

    updated banners

--- components/Layout/components/Banner/Carousel.jsx
+++ components/Layout/components/Banner/Carousel.jsx
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux'
 import karachiScrollingBanner1 from '../../../../public/assets/images/banners/homeDeliveryCarousel2.png'
 import karachiScrollingBanner2 from '../../../../public/assets/images/banners/homeDeliveryCarousel3.png'
 import karachiScrollingBanner3 from '../../../../public/assets/images/banners/7 Main scrollable Banner.png'
+import karachiScrollingBanner4 from '../../../../public/assets/images/banners/1+Main+Scrollable+(Website+Banner).png'
 import peshawarScrollingBanner from '../../../../public/assets/images/banners/peshawarBulkBuyCarousel3.png'
 import bulkBuyBannerKarachi from '../../../../public/assets/images/banners/webscrollablebannerBBD&D.png'
 import bulkBuyBanner from '../../../../public/assets/images/banners/bulkBuyCarousel.png'
@@ -55,6 +56,17 @@ const Carousel = () => {
                     )}
                     {selectedTypeSelector === 'home' && (
                         <Slider {...settings}>
+                            <div className="w-full h-full">
+                                <Link href={'/offers/103'} passHref>
+                                    <a className="w-full h-full">
+                                        <Image
+                                            src={karachiScrollingBanner4}
+                                            layout={"responsive"}
+                                            alt="banner"
+                                        />
+                                    </a>
+                                </Link>
+                            </div>
                             <div className="w-full h-full">
                                 <Link href={'/offers/81'} passHref>
                                     <a className="w-full h-full">
