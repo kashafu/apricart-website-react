3a4ae4050cbc9e330674ce90927251a11b5fe2a9
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-11-07 13:30:25 +0500

    made cartdetails shimmer better

--- components/Layout/components/Loaders/Shimmers/CartDetailsShimmer.js
+++ components/Layout/components/Loaders/Shimmers/CartDetailsShimmer.js
@@ -1,15 +1,15 @@
 const CartDetailsShimmer = () => {
     const SingleShimmer = () => {
         return (
-            <div className="w-full h-full flex flex-row justify-between animate-pulse">
-                <div className="w-1/4 h-full rounded-lg bg-slate-100" />
+            <div className="w-full h-full py-1 flex flex-row justify-between animate-pulse">
+                <div className="w-1/5 h-full rounded-lg bg-slate-100" />
                 <div className="w-1/3 h-full rounded-lg bg-slate-100" />
             </div>
         )
     }
 
     return (
-        <div className="flex flex-col w-full h-[220px] bg-slate-200 p-2 rounded-lg space-y-4">
+        <div className="flex flex-col w-full lg:border-l-2 border-y-2 h-[220px] p-2 space-y-4">
             <SingleShimmer />
             <SingleShimmer />
             <SingleShimmer />
