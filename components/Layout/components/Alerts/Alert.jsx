4af925735b73b855a2812992db6f2c7b4995f7d7
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-11-30 13:01:28 +0500

    alert responsive now

--- components/Layout/components/Alerts/Alert.jsx
+++ components/Layout/components/Alerts/Alert.jsx
@@ -3,7 +3,7 @@ import SubmitButton from "../Buttons/SubmitButton";
 const Alert = ({ text, onClick }) => {
     return (
         <div className="animate-slide-up fixed inset-0 h-full w-full backdrop-blur-sm z-50">
-            <div className="fixed w-full lg:w-1/3 bg-white border-t-2 border-main-red shadow-2xl bottom-0 z-50 rounded-t-3xl p-2">
+            <div className="fixed w-full lg:w-1/3 bg-white border-t-2 border-main-red shadow-2xl bottom-0 lg:inset-0 lg:m-auto lg:h-fit lg:rounded-3xl lg:border-2 z-50 rounded-t-3xl p-2">
                 <div className="h-3/4 w-full flex items-center justify-center py-16">
                     <p className="font-nunito text-black text-2xl">
                         {text}
