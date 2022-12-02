3d002125222573da9a31cd1fde13b62c76bf1221
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2022-12-02 09:58:59 +0500

    alert added for changing order type

--- components/Layout/components/Alerts/Alert.jsx
+++ components/Layout/components/Alerts/Alert.jsx
@@ -4,8 +4,8 @@ const Alert = ({ text, onClick }) => {
     return (
         <div className="animate-slide-up fixed inset-0 h-full w-full backdrop-blur-sm z-50">
             <div className="fixed w-full lg:w-1/3 bg-white border-t-2 border-main-red shadow-2xl bottom-0 lg:inset-0 lg:m-auto lg:h-fit lg:rounded-3xl lg:border-2 z-50 rounded-t-3xl p-2">
-                <div className="h-3/4 w-full flex items-center justify-center py-16">
-                    <p className="font-nunito text-black text-2xl">
+                <div className="h-3/4 w-full flex items-center justify-center py-16 lg:py-8">
+                    <p className="font-nunito text-black text-2xl text-center">
                         {text}
                     </p>
                 </div>
--- components/Layout/components/Cards/TypeCardSelector.js
+++ components/Layout/components/Cards/TypeCardSelector.js
@@ -5,14 +5,17 @@ import Image from "next/image"
 import ClickAndCollectCard from "./ClickAndCollectCard"
 import BulkBuyCard from "./BulkBuyCard"
 import { updateSelectedType } from "../../../../redux/general.slice"
+import SubmitButton from "../Buttons/SubmitButton"
 
 import dropdownSVG from "../../../../public/assets/svgs/dropdownArrow.svg"
+import Alert from "../Alerts/Alert"
 
 const TypeCardSelector = () => {
     const dispatch = useDispatch()
     const citySelector = useSelector((state) => state.general.city)
     const selectedTypeSelector = useSelector((state) => state.general.selectedType)
     const [isShow, setIsShow] = useState(false)
+    const [isShowAlert, setIsShowAlert] = useState(false)
 
     useEffect(() => {
         if (citySelector === 'peshawar') {
@@ -22,48 +25,68 @@ const TypeCardSelector = () => {
 
     /*
         Hide dropdown whenever new type is selected
+        Show alert if there are items in cart
     */
     useEffect(() => {
         setIsShow(false)
+        setIsShowAlert(true)
     }, [selectedTypeSelector])
 
-    return (
-        <div className="flex flex-col w-[100px] lg:w-[165px]">
-            <button className="w-full h-[40px] lg:h-[50px] bg-main-blue rounded-md flex flex-row items-center justify-between px-1 lg:px-2"
-                onClick={() => {
-                    setIsShow(!isShow)
-                }}
-            >
-                <p className="font-nunito text-[10px] lg:text-base font-bold text-white">
-                    {selectedTypeSelector === 'bulk' && "Online Delivery"}
-                    {selectedTypeSelector === 'cnc' && "Click & Collect"}
-                </p>
-                <div className="relative h-[8px] w-[10px] lg:h-[10px] lg:w-[12px]">
-                    <Image
-                        className={isShow ? "rotate-180 duration-300 ease-in-out" : "rotate-0 duration-300 ease-in-out"}
-                        src={dropdownSVG}
-                        alt='icon'
-                        layout="fill"
+    const AlertBox = () => {
+        return (
+            <>
+                {isShowAlert && (
+                    <Alert
+                        text={"Attention: Some of the selected products may not be available if you change order type."}
+                        onClick={() => {
+                            setIsShowAlert(false)
+                        }}
                     />
-                </div>
-            </button>
-            {isShow && (
-                <>
-                    {citySelector === 'karachi' && (
-                        <div className="animate-dropdown w-[100px] lg:w-[165px] absolute z-50 top-[40px] lg:top-[50px] bg-[#F0F0F0] border-b-2 border-x-2 border-main-blue py-2">
-                            <>
-                                {selectedTypeSelector === 'bulk' && (
-                                    <ClickAndCollectCard />
-                                )}
-                                {selectedTypeSelector === 'cnc' && (
-                                    <BulkBuyCard />
-                                )}
-                            </>
-                        </div>
-                    )}
-                </>
-            )}
-        </div>
+                )}
+            </>
+        )
+    }
+
+    return (
+        <>
+            <AlertBox />
+            <div className="flex flex-col w-[100px] lg:w-[165px]">
+                <button className="w-full h-[40px] lg:h-[50px] bg-main-blue rounded-md flex flex-row items-center justify-between px-1 lg:px-2"
+                    onClick={() => {
+                        setIsShow(!isShow)
+                    }}
+                >
+                    <p className="font-nunito text-[10px] lg:text-base font-bold text-white">
+                        {selectedTypeSelector === 'bulk' && "Online Delivery"}
+                        {selectedTypeSelector === 'cnc' && "Click & Collect"}
+                    </p>
+                    <div className="relative h-[8px] w-[10px] lg:h-[10px] lg:w-[12px]">
+                        <Image
+                            className={isShow ? "rotate-180 duration-300 ease-in-out" : "rotate-0 duration-300 ease-in-out"}
+                            src={dropdownSVG}
+                            alt='icon'
+                            layout="fill"
+                        />
+                    </div>
+                </button>
+                {isShow && (
+                    <>
+                        {citySelector === 'karachi' && (
+                            <div className="animate-dropdown w-[100px] lg:w-[165px] absolute z-50 top-[40px] lg:top-[50px] bg-[#F0F0F0] border-b-2 border-x-2 border-main-blue py-2">
+                                <>
+                                    {selectedTypeSelector === 'bulk' && (
+                                        <ClickAndCollectCard />
+                                    )}
+                                    {selectedTypeSelector === 'cnc' && (
+                                        <BulkBuyCard />
+                                    )}
+                                </>
+                            </div>
+                        )}
+                    </>
+                )}
+            </div>
+        </>
     )
 }
 
