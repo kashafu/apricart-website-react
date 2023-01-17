847e146626425abb70d68e5501bc4abd923a5427
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2023-01-17 14:04:41 +0500

    zindigi logo shows on footer accordingly

--- components/Layout/components/Footer/Footer.js
+++ components/Layout/components/Footer/Footer.js
@@ -1,30 +1,35 @@
-import { useState } from "react";
-import phoneIcon from "../../../../public/assets/svgs/phoneIcon.svg";
-import globeIcon from "../../../../public/assets/svgs/globeIcon.svg";
-import playstoreImg from "../../../../public/assets/images/playstore-img.png";
-import appstoreImg from "../../../../public/assets/images/appstore-img.png";
-import meezanIcon from "../../../../public/assets/images/meezan-icon.png";
-import visaIcon from "../../../../public/assets/images/visa-icon.png";
-import masterIcon from "../../../../public/assets/images/mastercard-icon.png";
-import codIcon from "../../../../public/assets/images/cod-icon.png";
+import Image from "next/image"
+import Link from "next/link"
+import { useSelector } from "react-redux"
 
-import Image from "next/image";
-import Link from "next/link";
+import phoneIcon from "../../../../public/assets/svgs/phoneIcon.svg"
+import globeIcon from "../../../../public/assets/svgs/globeIcon.svg"
+import playstoreImg from "../../../../public/assets/images/playstore-img.png"
+import appstoreImg from "../../../../public/assets/images/appstore-img.png"
+import meezanIcon from "../../../../public/assets/images/meezan-icon.png"
+import visaIcon from "../../../../public/assets/images/visa-icon.png"
+import masterIcon from "../../../../public/assets/images/mastercard-icon.png"
+import codIcon from "../../../../public/assets/images/cod-icon.png"
+import zindigiLogo from "../../../../public/assets/images/zindigiLogo.png"
 
 const Footer = () => {
+	const redirectSourceSelector = useSelector((state) => state.general.redirectSource)
+
 	return (
 		<>
 			<footer className="hidden sm:flex flex-col bg-main-grey-1000 py-12">
 				<section className="flex justify-between w-full ">
 					<section className="w-1/3 flex flex-col">
 						<div className="pl-4 pb-4">
-							<p className=" text-white font-bold">Contact Details</p>
+							<p className=" text-white font-bold">
+								Contact Details
+							</p>
 						</div>
 						<div className="px-2 inline-flex mb-2">
 							<p className=" px-2 text-main-grey-500 font-lato text-sm">
-								<strong>Address: </strong>Office No 106, Emarah Suites, Main
-								Shahrah-e-Faisal, Sindhi Muslim Co-operative Housing Society,
-								Karachi
+								<strong>Address: </strong>Office No 106, Emarah
+								Suites, Main Shahrah-e-Faisal, Sindhi Muslim
+								Co-operative Housing Society, Karachi
 							</p>
 						</div>
 					</section>
@@ -34,12 +39,17 @@ const Footer = () => {
 						<div className="col-span-3">
 							<div className="mb-2 ">
 								<p className="text-main-grey-500 font-lato text-sm">
-									<strong className="text-white">Phone: </strong>0304-1110195
+									<strong className="text-white">
+										Phone:{" "}
+									</strong>
+									0304-1110195
 								</p>
 							</div>
 							<div className=" mb-4">
 								<p className="text-main-grey-500 font-lato text-sm">
-									<strong className="text-white">Email: </strong>
+									<strong className="text-white">
+										Email:{" "}
+									</strong>
 									support@apricart.pk
 								</p>
 							</div>
@@ -87,7 +97,9 @@ const Footer = () => {
 						<div className="flex justify-evenly items-center w-full">
 							<Link
 								passHref
-								href={"https://www.facebook.com/apricartonlinegrocery/"}
+								href={
+									"https://www.facebook.com/apricartonlinegrocery/"
+								}
 							>
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
@@ -99,7 +111,9 @@ const Footer = () => {
 							</Link>
 							<Link
 								passHref
-								href={"https://twitter.com/apricartpk?lang=en    /"}
+								href={
+									"https://twitter.com/apricartpk?lang=en    /"
+								}
 							>
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
@@ -111,7 +125,9 @@ const Footer = () => {
 							</Link>
 							<Link
 								passHref
-								href={"https://pk.linkedin.com/company/apricartestores/"}
+								href={
+									"https://pk.linkedin.com/company/apricartestores/"
+								}
 							>
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
@@ -123,7 +139,9 @@ const Footer = () => {
 							</Link>
 							<Link
 								passHref
-								href={"https://www.instagram.com/apricart.pk/?hl=en/"}
+								href={
+									"https://www.instagram.com/apricart.pk/?hl=en/"
+								}
 							>
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
@@ -137,38 +155,53 @@ const Footer = () => {
 					</section>
 					{/* Bangks */}
 					<section className="flex justify-center items-center w-1/3 px-2">
-						<Link passHref href={""}>
-							<Image
-								src={meezanIcon}
-								alt={"Meezan Icon"}
-								width={"101px"}
-								height={"30px"}
-							/>
-						</Link>
-						<Link passHref href={""}>
-							<Image
-								src={visaIcon}
-								alt={"Visa Icon"}
-								width={"100px"}
-								height={"50px"}
-							/>
-						</Link>
-						<Link passHref href={""} className="">
-							<Image
-								src={masterIcon}
-								alt={"Visa Icon"}
-								width={"100px"}
-								height={"50px"}
-							/>
-						</Link>
-						<Link passHref href={""}>
-							<Image
-								src={codIcon}
-								alt={"Visa Icon"}
-								width={"60px"}
-								height={"30px"}
-							/>
-						</Link>
+						{redirectSourceSelector === 'js_bank' ? (
+							<>
+								<Link passHref href={""}>
+									<Image
+										src={zindigiLogo}
+										alt={"zindigi Logo"}
+										width={"101px"}
+										height={"30px"}
+									/>
+								</Link>
+							</>
+						) : (
+							<>
+								<Link passHref href={""}>
+									<Image
+										src={meezanIcon}
+										alt={"Meezan Icon"}
+										width={"101px"}
+										height={"30px"}
+									/>
+								</Link>
+								<Link passHref href={""}>
+									<Image
+										src={visaIcon}
+										alt={"Visa Icon"}
+										width={"100px"}
+										height={"50px"}
+									/>
+								</Link>
+								<Link passHref href={""} className="">
+									<Image
+										src={masterIcon}
+										alt={"Visa Icon"}
+										width={"100px"}
+										height={"50px"}
+									/>
+								</Link>
+								<Link passHref href={""}>
+									<Image
+										src={codIcon}
+										alt={"Visa Icon"}
+										width={"60px"}
+										height={"30px"}
+									/>
+								</Link>
+							</>
+						)}
 					</section>
 					{/* DOwnload ABB mobile*/}
 					<section className="w-1/3">
@@ -212,7 +245,7 @@ const Footer = () => {
 						</div>
 					</section>
 				</section>
-			</footer>
+			</footer >
 
 			{/* Phone  */}
 			{/*  */}
@@ -221,7 +254,9 @@ const Footer = () => {
 				<section className="flex flex-col sm:flex-row justify-between w-full ">
 					<section className=" flex flex-col ">
 						<div className="pl-4 pb-4">
-							<p className=" text-white font-bold">Contact Details</p>
+							<p className=" text-white font-bold">
+								Contact Details
+							</p>
 						</div>
 						<div className="px-2 inline-flex mb-2">
 							<div className="hidden md:inline-flex">
@@ -234,23 +269,37 @@ const Footer = () => {
 								</svg>
 							</div>
 							<p className=" px-2 text-main-grey-500 font-lato sm:text-xl text-sm">
-								<strong className="text-white">Address: </strong>Office No 601,
-								Emarah Suites, Main Shahrah-e-Faisal, Sindhi Muslim Co-operative
+								<strong className="text-white">
+									Address:{" "}
+								</strong>
+								Office No 601, Emarah Suites, Main
+								Shahrah-e-Faisal, Sindhi Muslim Co-operative
 								Housing Society, Karachi
 							</p>
 						</div>
 						<div className=" px-2 inline-flex mb-2">
 							<div className="hidden md:inline-flex">
-								<Image src={phoneIcon} width={22} height={22} alt="" />
+								<Image
+									src={phoneIcon}
+									width={22}
+									height={22}
+									alt=""
+								/>
 							</div>
 							<p className=" px-2 text-main-grey-500 font-lato sm:text-xl text-sm">
-								<strong className="text-white">Phone: </strong>0304-1110195
+								<strong className="text-white">Phone: </strong>
+								0304-1110195
 							</p>
 						</div>
 
 						<div className="px-2 inline-flex mb-4">
 							<div className="hidden md:inline-flex">
-								<Image src={globeIcon} width={25} height={25} alt="" />
+								<Image
+									src={globeIcon}
+									width={25}
+									height={25}
+									alt=""
+								/>
 							</div>
 							<p className="px-2 text-main-grey-500 font-lato sm:text-xl text-sm">
 								<strong className="text-white">Email: </strong>
@@ -263,7 +312,9 @@ const Footer = () => {
 						{/* About Us Mobile */}
 						<div className="w-1/2 text-left sm:hidden px-3 flex flex-col text-main-grey-500 font-lato">
 							<div>
-								<p className=" text-white font-bold">Quick Links</p>
+								<p className=" text-white font-bold">
+									Quick Links
+								</p>
 							</div>
 							<div className="flex flex-col mt-2 md:mt-0">
 								<Link passHref href={"/"}>
@@ -304,7 +355,9 @@ const Footer = () => {
 							<div className="flex justify-center items-center w-full">
 								<Link
 									passHref
-									href={"https://www.facebook.com/apricartonlinegrocery/"}
+									href={
+										"https://www.facebook.com/apricartonlinegrocery/"
+									}
 								>
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -316,7 +369,9 @@ const Footer = () => {
 								</Link>
 								<Link
 									passHref
-									href={"https://twitter.com/apricartpk?lang=en    /"}
+									href={
+										"https://twitter.com/apricartpk?lang=en    /"
+									}
 								>
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -328,7 +383,9 @@ const Footer = () => {
 								</Link>
 								<Link
 									passHref
-									href={"https://pk.linkedin.com/company/apricartestores/"}
+									href={
+										"https://pk.linkedin.com/company/apricartestores/"
+									}
 								>
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -340,7 +397,9 @@ const Footer = () => {
 								</Link>
 								<Link
 									passHref
-									href={"https://www.instagram.com/apricart.pk/?hl=en/"}
+									href={
+										"https://www.instagram.com/apricart.pk/?hl=en/"
+									}
 								>
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -395,44 +454,59 @@ const Footer = () => {
 						</section>
 					</div>
 					{/* Bangks */}
-					<div className="flex justify-center items-center w-full px-2">
-						<Link passHref href={""}>
-							<Image
-								src={meezanIcon}
-								alt={"Meezan Icon"}
-								width={"101px"}
-								height={"30px"}
-							/>
-						</Link>
-						<Link passHref href={""}>
-							<Image
-								src={visaIcon}
-								alt={"Visa Icon"}
-								width={"100px"}
-								height={"50px"}
-							/>
-						</Link>
-						<Link passHref href={""} className="mx-4">
-							<Image
-								src={masterIcon}
-								alt={"Visa Icon"}
-								width={"100px"}
-								height={"50px"}
-							/>
-						</Link>
-						<Link passHref href={""}>
-							<Image
-								src={codIcon}
-								alt={"Visa Icon"}
-								width={"60px"}
-								height={"30px"}
-							/>
-						</Link>
+					<div className="flex justify-center items-center w-full px-2 pt-6">
+						{redirectSourceSelector === 'js_bank' ? (
+							<>
+								<Link passHref href={""}>
+									<Image
+										src={zindigiLogo}
+										alt={"zindigi Logo"}
+										width={"101px"}
+										height={"30px"}
+									/>
+								</Link>
+							</>
+						) : (
+							<>
+								<Link passHref href={""}>
+									<Image
+										src={meezanIcon}
+										alt={"Meezan Icon"}
+										width={"101px"}
+										height={"30px"}
+									/>
+								</Link>
+								<Link passHref href={""}>
+									<Image
+										src={visaIcon}
+										alt={"Visa Icon"}
+										width={"100px"}
+										height={"50px"}
+									/>
+								</Link>
+								<Link passHref href={""} className="mx-4">
+									<Image
+										src={masterIcon}
+										alt={"Visa Icon"}
+										width={"100px"}
+										height={"50px"}
+									/>
+								</Link>
+								<Link passHref href={""}>
+									<Image
+										src={codIcon}
+										alt={"Visa Icon"}
+										width={"60px"}
+										height={"30px"}
+									/>
+								</Link>
+							</>
+						)}
 					</div>
 				</section>
 			</footer>
 		</>
-	);
-};
+	)
+}
 
-export default Footer;
+export default Footer
