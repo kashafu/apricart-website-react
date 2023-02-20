3b80ba4aa6b51a8001fbf1af6eba3da1fc6d499f
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2023-02-20 11:20:07 +0500

    updated order view

--- pages/orders.js
+++ pages/orders.js
@@ -5,137 +5,102 @@ import ProfileNavigationMenuAndItemsLayout from "../components/Layout/components
 import { useOptionsApi, useOrderHistoryApi } from "../helpers/Api"
 import SingleOrderRow from "../components/Layout/components/Orders/SingleOrderRow"
 
-const Orders = () => {
-	const AllOrders = () => {
-		const [selectedTab, setSelectedTab] = useState('pending')
-		const { isLoading, errorResponse, errorMessage, pendingOrders, cancelledOrders, completedOrders } = useOrderHistoryApi()
-		const { isLoading: optionsIsLoading, orderCancelTime } = useOptionsApi()
+const AllOrders = () => {
+	const [selectedTab, setSelectedTab] = useState('pending')
+	const { isLoading, errorResponse, errorMessage, pendingOrders, cancelledOrders, completedOrders } = useOrderHistoryApi()
+	const { isLoading: optionsIsLoading, orderCancelTime } = useOptionsApi()
 
-		const Tab = () => {
-			return (
-				<div className="flex flex-row w-full">
-					<button className={"w-1/3 bg-white font-nunito text-main-blue font-bold text-lg border-2 border-main-blue " + [selectedTab === 'pending' && "text-white bg-main-blue"]}
-						onClick={() => {
-							setSelectedTab('pending')
-						}}
-					>
-						PENDING
-					</button>
-					<button className={"w-1/3 bg-white font-nunito text-main-blue font-bold text-lg border-2 border-main-blue " + [selectedTab === 'completed' && "text-white bg-main-blue"]}
-						onClick={() => {
-							setSelectedTab('completed')
-						}}
-					>
-						COMPLETED
-					</button>
-					<button className={"w-1/3 bg-white font-nunito text-main-blue font-bold text-lg border-2 border-main-blue " + [selectedTab === 'cancelled' && "text-white bg-main-blue"]}
-						onClick={() => {
-							setSelectedTab('cancelled')
-						}}
-					>
-						CANCELLED
-					</button>
-
-				</div>
-			)
-		}
-
-		if (isLoading || optionsIsLoading) {
-			return <></>
-		}
-
-		if (errorResponse) {
-			return (
-				<p>
-					{errorMessage}
-				</p>
-			)
-		}
-
+	const Tab = () => {
 		return (
-			<div className="w-full">
-				<Tab />
-				<div className="overflow-auto">
-					<table className="w-full table-auto table border-separate">
-						{selectedTab === 'pending' && (
-							<>
-								<thead>
-									<tr className="text-center truncate">
-										<th>Order Id</th>
-										<th>Order Date</th>
-										<th>Items</th>
-										<th>Amount</th>
-										<th>Order Type</th>
-									</tr>
-								</thead>
-								<tbody>
-									{pendingOrders.map((order) => {
-										let { orderId } = order
-										return (
-											<SingleOrderRow
-												key={orderId}
-												order={order}
-												cancelTime={orderCancelTime}
-											/>
-										)
-									})}
-								</tbody>
-							</>
-						)}
-						{selectedTab === 'completed' && (
-							<>
-								<thead>
-									<tr className="text-center truncate">
-										<th>Order Id</th>
-										<th>Order Date</th>
-										<th>Total Items</th>
-										<th>Total Amount</th>
-										<th>Order Type</th>
-									</tr>
-								</thead>
-								<tbody>
-									{completedOrders.map((order) => {
-										let { orderId } = order
-										return (
-											<SingleOrderRow
-												key={orderId}
-												order={order}
-											/>
-										)
-									})}
-								</tbody>
-							</>
-						)}
-						{selectedTab === 'cancelled' && (
-							<>
-								<thead>
-									<tr className="text-center truncate">
-										<th>Order Id</th>
-										<th>Order Date</th>
-										<th>Total Items</th>
-										<th>Total Amount</th>
-										<th>Order Type</th>
-									</tr>
-								</thead>
-								<tbody>
-									{cancelledOrders.map((order) => {
-										let { orderId } = order
-										return (
-											<SingleOrderRow
-												key={orderId}
-												order={order}
-											/>
-										)
-									})}
-								</tbody>
-							</>
-						)}
-					</table>
-				</div>
+			<div className="flex flex-row w-full">
+				<button className={"w-1/3 bg-white font-nunito text-main-blue font-bold text-lg border-2 border-main-blue " + [selectedTab === 'pending' && "text-white bg-main-blue"]}
+					onClick={() => {
+						setSelectedTab('pending')
+					}}
+				>
+					PENDING
+				</button>
+				<button className={"w-1/3 bg-white font-nunito text-main-blue font-bold text-lg border-2 border-main-blue " + [selectedTab === 'completed' && "text-white bg-main-blue"]}
+					onClick={() => {
+						setSelectedTab('completed')
+					}}
+				>
+					COMPLETED
+				</button>
+				<button className={"w-1/3 bg-white font-nunito text-main-blue font-bold text-lg border-2 border-main-blue " + [selectedTab === 'cancelled' && "text-white bg-main-blue"]}
+					onClick={() => {
+						setSelectedTab('cancelled')
+					}}
+				>
+					CANCELLED
+				</button>
+
 			</div>
 		)
 	}
 
+	if (isLoading || optionsIsLoading) {
+		return <></>
+	}
+
+	if (errorResponse) {
+		return (
+			<p>
+				{errorMessage}
+			</p>
+		)
+	}
+
+	return (
+		<div className="w-full flex flex-col">
+			<Tab />
+			<section className="w-full flex flex-col space-y-2 divide-y-2">
+				{selectedTab === 'pending' && (
+					<>
+						{pendingOrders.map((order) => {
+							let { orderId } = order
+							return (
+								<SingleOrderRow
+									key={orderId}
+									order={order}
+									cancelTime={orderCancelTime}
+								/>
+							)
+						})}
+					</>
+				)}
+				{selectedTab === 'completed' && (
+					<>
+						{completedOrders.map((order) => {
+							let { orderId } = order
+							return (
+								<SingleOrderRow
+									key={orderId}
+									order={order}
+								/>
+							)
+						})}
+					</>
+				)}
+				{selectedTab === 'cancelled' && (
+					<>
+						{cancelledOrders.map((order) => {
+							let { orderId } = order
+							return (
+								<SingleOrderRow
+									key={orderId}
+									order={order}
+								/>
+							)
+						})}
+					</>
+				)}
+			</section>
+		</div>
+	)
+}
+
+const Orders = () => {
 	return (
 		<div>
 			<HeadTag title={"Orders"} />
